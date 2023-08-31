71ec432532862ee888ca8f19487344f93ae5b396
unknown <mhdfavascheru@gmail.com>
2023-08-31 18:07:53 +0530

    updated news carousel

--- src/components/carousol/CarouselItem.jsx
+++ src/components/carousol/CarouselItem.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import './newsCarousol.css'
-export const CarouselItem = ({ item, width,isSelected }) => {
+export const CarouselItem = ({ item, width, isSelected }) => {
+  console.log(item);
   return (
-    <div className="carousel-item1" style={{ width: width,opacity:isSelected?1:0 }}>
+    <div className="carousel-item1" style={{ width: width, opacity: isSelected ? 1 : 0 }}>
       {/* <div></div> */}
       {
         item.image_url
-        ?
-        <img className="carousel-img" src={item.image_url} />
-        :null
+          ?
+          <img className="carousel-img" src={item.image_url} />
+          : null
       }
-      <div className="carousel-item-text">{item.description}</div>
+      <div style={{position:'relative'}}>
+        <div className="carousel-item-text">
+          {item.description}
+        </div>
+        <a href={item.link} style={{ position: 'absolute', bottom: '-15px', left: '30px' }}>
+          read More
+        </a>
+      </div>
     </div>
   );
 };
